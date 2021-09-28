fd0dddf16510d64c14b34489186c02336dd3249c
3rivelton <erivelton.r.alves@gmail.com>
2021-09-27 23:02:39 -0300

    service and repository

---
+++ src/main/java/com/erivelton/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.erivelton.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.erivelton.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+	
+	
+}
