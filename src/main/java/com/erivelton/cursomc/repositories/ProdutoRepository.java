a09cf709c264a93033fb3ecec6d89efa881b18b4
3rivelton <erivelton.r.alves@gmail.com>
2021-09-29 22:02:57 -0300

    associacao muitos para muitos e criação de produtos

---
+++ src/main/java/com/erivelton/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.erivelton.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.erivelton.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	
+	
+}
