0fbb155ef5132975b2c1707b476ace03c6b6f0c2
3rivelton <erivelton.r.alves@gmail.com>
2021-10-04 23:25:54 -0300

    pedido pagamento e etcs

---
+++ src/main/java/com/erivelton/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.erivelton.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.erivelton.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {}
