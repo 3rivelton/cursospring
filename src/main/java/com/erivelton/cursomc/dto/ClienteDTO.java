fa64c6472320878ccf922320d0f3798328f12ad6
3rivelton <erivelton.r.alves@gmail.com>
2021-11-11 21:48:06 -0300

    validação email repetido atualização

--- src/main/java/com/erivelton/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/erivelton/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.erivelton.cursomc.domain.Cliente;
+import com.erivelton.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
