ca3bdde89a303540af2db428f4ee783a3e53458b
3rivelton <erivelton.r.alves@gmail.com>
2021-12-03 22:26:22 -0300

    criando e salvando perfis na base de dados

--- src/main/java/com/erivelton/cursomc/services/DbService.java
+++ src/main/java/com/erivelton/cursomc/services/DbService.java
@@ -20,6 +20,7 @@ import com.erivelton.cursomc.domain.PagamentoComCartao;
 import com.erivelton.cursomc.domain.Pedido;
 import com.erivelton.cursomc.domain.Produto;
 import com.erivelton.cursomc.domain.enums.EstadoPagamento;
+import com.erivelton.cursomc.domain.enums.Perfil;
 import com.erivelton.cursomc.domain.enums.TipoCliente;
 import com.erivelton.cursomc.repositories.CategoriaRepository;
 import com.erivelton.cursomc.repositories.CidadeRepository;
@@ -116,14 +117,19 @@ public class DbService {
 		Cliente cli1 = new Cliente(null, "Maria Silvia", "erivelton.r.alves@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "3822083", cli1, c1);
+		Cliente cli2 = new Cliente(null, "Ana Costa", "ersa@a.recife.ifpe.edu.br", "67394784061", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli1.getTelefones().addAll(Arrays.asList("19993323", "93833838"));
+		cli2.addPerfil(Perfil.ADMIN);
 		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "3822083", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
